87b4d1b754b29e60af024cf4fd239e55d12dc338
AndoyoStefanie <89018831+AndoyoStefanie@users.noreply.github.com>
2021-09-20 14:02:07 +0800

    commit

---
+++ examjava.js
@@ -0,0 +1,97 @@
+let name = 'Baal';
+
+function Hello() {
+    let name = 'Sarah';
+    return 'Hello' + name;
+
+
+}
+console.log(Hello)
+
+/*The output of the following code is 'Hello' because the
+local variable takes precedence over the global
+variable name*/
+
+
+
+
+
+
+/*callback function allows an object or a variable in one function
+to be used by other functions of the code*/
+
+function getStud(id) {
+    alert('Get' + id);
+}
+
+function inputId(callback) {
+    var id = prompt('Please enter your ID:');
+    callback(id)
+}
+inputId(getStud);
+
+/*The variable name in the function inputId is used on the other function
+in the getStud code.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (var i=0; i<=15; i++) {  /*the variable is named i which has a value set to 0,
+    since the problem starrts at zero. A condition is also specified
+    which allows the incrementation only until 15. The i++ allows the code
+    to increment by 1 until it returns false*/
+
+
+    if (i === 0) {
+        console.log(i + "is even"); /*this checks if i is equal to 0 */
+    }
+else if (x % 2 === 0) {
+    console.log(i + "is even");
+/*checks if i has a remainder of 0, if true it will execute*/
+}
+else {
+    console.log(i + "is odd"); /*if both are false, the else condition will execute,
+    but if not the console.log will execute.*/
+}
+}
+
+
+
+
+
+
+function bubble_Sort(a)
+{
+    var swapp;
+    var n = a.length-1;
+    var x=a;
+    do {
+        swapp = false;
+        for (var i=0; i < n; i++)
+        {
+            if (x[i] < x[i+1])
+            {
+               var temp = x[i];
+               x[i] = x[i+1];
+               x[i+1] = temp;
+               swapp = true;
+            }
+        }
+        n--;
+    } while (swapp);
+ return x; 
+}
+
+console.log(bubble_Sort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
+
